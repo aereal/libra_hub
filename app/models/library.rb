b0df3aefc4be667569aa4b7c5b900fccebb3f79b
aereal <aereal@kerare.org>
2012-04-02 01:01:06 +0900

    Library#name should be unique

--- app/models/library.rb
+++ app/models/library.rb
@@ -2,5 +2,5 @@ class Library < ActiveRecord::Base
   belongs_to :language
   attr_accessible :name, :slug
 
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
 end
--- spec/models/library_spec.rb
+++ spec/models/library_spec.rb
@@ -13,5 +13,17 @@ describe Library do
     context 'has name' do
       it { should_not have(1).error_on(:name) }
     end
+
+    context 'with unique name' do
+      let!(:another_library) { FactoryGirl.build(:library) }
+
+      it { should_not have(1).error_on(:name) }
+    end
+
+    context 'with duplicated name' do
+      let!(:another_library) { FactoryGirl.create(:library, name: subject.name) }
+
+      it { should have(1).error_on(:name) }
+    end
   end
 end
