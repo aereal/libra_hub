c28452dc65f1cf70dd4b91a78f21a199ed851de3
aereal <aereal@kerare.org>
2012-04-01 21:46:18 +0900

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LibraHub::Application.config.secret_token = 'cfe950179f7054988472f4bc65d8af632f4d2973b3bb22fff38b7605412d69677284232331d70cf933e7571ebc983c3327a2e147b2352aea2d0bdf03e826c363'
